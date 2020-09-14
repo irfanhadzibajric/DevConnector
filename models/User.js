67fb25fb9fb2106d06e4a2ca5d610d0e0960f440
irfanhadzibajric <62591559+irfanhadzibajric@users.noreply.github.com>
2020-09-14 10:20:12 +0200

    second commit

---
+++ models/User.js
@@ -0,0 +1,27 @@
+//kreiramo model za bazu podataka, potrebno je napraviti schemu
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model('users', UserSchema); //module name, schema
